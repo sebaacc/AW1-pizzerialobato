37b8d4f618e6b98aa9ab4bf93ea0623c35e54714
Sebastián-Markoja <sebaacc11@gmail.com>
2025-11-14 11:45:08 -0300

    menu hamburguesa con JS funcionando

--- recursos/js/inicio/index.js
+++ recursos/js/inicio/index.js
@@ -4,6 +4,19 @@ const $contenedorProductos = document.querySelector('#contenedor-productos')
 
 const $contenedorPromociones = document.querySelector('#contenedor-promos')
 
+const $nav = document.querySelector('.nav-principal')
+
+const $abrirMenuBtn = document.querySelector('.abrir-menu')
+
+const $cerrarMenuBtn = document.querySelector('.cerrar-menu')
+$abrirMenuBtn.addEventListener('click', () => {
+  $nav.classList.add('visible')
+})
+
+$cerrarMenuBtn.addEventListener('click', () => {
+  $nav.classList.remove('visible')
+})  
+
 const productos = await obtenerProductosJSON('http://127.0.0.1:5500/recursos/js/productos.json')
 
 const promociones = await obtenerProductosJSON('http://127.0.0.1:5500/recursos/js/promociones.json')
